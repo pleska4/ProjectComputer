3fe1025038c9087d60cc5bc47d3b3c2705bc88a4
Yauheni Pleskach <epleska4@gmail.com>
2022-04-10 14:37:01 +0300

    Signed-off-by: Yauheni Pleskach <epleska4@gmail.com>

--- src/main/java/computer/enums/TypeOfPrinter.java
+++ src/main/java/computer/enums/TypeOfPrinter.java
@@ -1,15 +1,20 @@
 package computer.enums;
 
 public enum TypeOfPrinter {
-    LASER,
-    MATRIX,
-    JET
-}
+    LASER("LASER"),
+    MATRIX("MATRIX"),
+    JET("JET");
+
     private String value;
-        
+
     TypeOfPrinter(String value) {
         this.value = value;
     }
-    public String getValue(){
+
+    public String getValue() {
         return value;
     }
+}
+
+
+
